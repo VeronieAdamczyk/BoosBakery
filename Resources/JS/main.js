5466ba958c0f70123ccc683903a9d86bcd0bdd13
VeronieAdamczyk <42538456+VeronieAdamczyk@users.noreply.github.com>
2018-10-09 18:25:39 +0100

    Add files via upload

---
+++ Resources/JS/main.js
@@ -0,0 +1,57 @@
+console.log("linked");
+
+// Mobile drop down menu
+function mobileMenu() {
+  var dropDown = document.getElementById("ddMenu");
+  if (dropDown.style.display === "block") {
+    dropDown.style.display = "none";
+  } else {
+    dropDown.style.display = "block";
+  }
+}
+
+// Product pop ups
+
+function prodPopUp1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+}
+function prodPopUp2() {
+    var popup = document.getElementById("myPopup2");
+    popup.classList.toggle("show");
+}
+
+function prodPopUp3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+}
+
+function prodPopUp4() {
+    var popup = document.getElementById("myPopup4");
+    popup.classList.toggle("show");
+}
+
+function prodPopUp5() {
+    var popup = document.getElementById("myPopup5");
+    popup.classList.toggle("show");
+}
+
+function prodPopUp6() {
+    var popup = document.getElementById("myPopup6");
+    popup.classList.toggle("show");
+}
+
+function prodPopUp7() {
+    var popup = document.getElementById("myPopup7");
+    popup.classList.toggle("show");
+}
+
+function prodPopUp8() {
+    var popup = document.getElementById("myPopup8");
+    popup.classList.toggle("show");
+}
+
+function prodPopUp9() {
+    var popup = document.getElementById("myPopup9");
+    popup.classList.toggle("show");
+}
